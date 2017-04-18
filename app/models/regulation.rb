0f6073b435685bb4201ce7720cf431cd68c96580
github username <github email address>
2017-04-18 10:11:48 -0400

    done

--- app/models/regulation.rb
+++ app/models/regulation.rb
@@ -3,6 +3,8 @@ class Regulation < ApplicationRecord
   has_many :votes
   has_many :citizens, through: :followers
   has_many :citizens, through: :comments
+  has_many :citizens, through: :votes
+
   belongs_to :category
 
 end
